a98849c6002d13726ecb23919fd72f11addc2c1e
Chinmoy Panwar <chinmoypanwar1@gmail.com>
2025-11-01 22:10:34 +0530

    second commit

--- src/API/axiosInstance.js
+++ src/API/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL,
+  baseURL: "https://cargofirst-backend.vercel.app/api",
   withCredentials: true,
 });
 
